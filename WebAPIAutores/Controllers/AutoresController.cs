3f25d1ab871578c4642c768d02422bac3228b934
steeltx <realsteel.gnu@gmail.com>
2021-07-28 22:00:49 -0500

    Controlador de autores

---
+++ WebAPIAutores/Controllers/AutoresController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIAutores.Entidades;
+
+namespace WebAPIAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>()
+            {
+                new Autor() { Id = 1, Nombre = "Felipe"},
+                new Autor() {Id = 2, Nombre = "Roberto"}
+            };
+        }
+    }
+}
