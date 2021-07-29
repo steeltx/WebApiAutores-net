a019f2bb1215ddaebe16288c4342169a8da35e61
steeltx <realsteel.gnu@gmail.com>
2021-07-28 23:44:30 -0500

    Eliminar y actualizar autores

--- WebAPIAutores/Controllers/AutoresController.cs
+++ WebAPIAutores/Controllers/AutoresController.cs
@@ -32,5 +32,40 @@ namespace WebAPIAutores.Controllers
             await context.SaveChangesAsync();
             return Ok();
         }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(Autor autor, int id)
+        {
+            if(autor.Id != id)
+            {
+                return BadRequest("El ID del autor no coincide con el id de la url");
+            }
+
+            /*
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+            if (!existe)
+            {
+                return NotFound();
+            }*/
+
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            /*
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+            if (!existe)
+            {
+                return NotFound();
+            }*/
+
+            context.Remove(new Autor() { Id = id });
+            await context.SaveChangesAsync();
+            return Ok();
+        }
     }
 }
