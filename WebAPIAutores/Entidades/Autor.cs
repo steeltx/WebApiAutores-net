795c8f2a0b9e803127c9cc5176b13e3abc96c384
steeltx <realsteel.gnu@gmail.com>
2021-08-07 10:01:39 -0500

    Validaciones de campos por modelo

--- WebAPIAutores/Entidades/Autor.cs
+++ WebAPIAutores/Entidades/Autor.cs
@@ -8,22 +8,45 @@ using WebAPIAutores.Validaciones;
 
 namespace WebAPIAutores.Entidades
 {
-    public class Autor
+    public class Autor : IValidatableObject
     {
         public int Id { get; set; }
         [Required(ErrorMessage ="El campo {0} es requerido")]
         [StringLength(maximumLength:4, ErrorMessage ="El campo {0} no debe de tener mas de {1} caracteres")]
-        [PrimeraLetraMayuscula]
+        // [PrimeraLetraMayuscula]
         public string Nombre { get; set; }
-        [Range(18,120)]
-        [NotMapped]
-        public int Edad { get; set; }
-        [CreditCard]
-        [NotMapped]
-        public string TarjetaCredito { get; set; }
-        [Url]
-        [NotMapped]
-        public string URL { get; set; }
+        //[Range(18,120)]
+        //[NotMapped]
+        //public int Edad { get; set; }
+        //[CreditCard]
+        //[NotMapped]
+        //public string TarjetaCredito { get; set; }
+        //[Url]
+        //[NotMapped]
+        //public string URL { get; set; }
+
+        public int Menor { get; set; }
+        public int Mayor { get; set; }
         public List<Libro> Libros { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Nombre))
+            {
+                var primeraLetra = Nombre[0].ToString();
+                if(primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayuscula", 
+                        new string[] { nameof(Nombre)});
+
+                }
+            }
+
+            if(Menor > Mayor)
+            {
+                yield return new ValidationResult("Este valor no puede ser mas grande que el campo mayor",
+                    new string[] { nameof(Menor) });
+            }
+        }
     }
 }
