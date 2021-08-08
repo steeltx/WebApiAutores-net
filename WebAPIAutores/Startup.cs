741b4906aa99851943b10b61436a3f4ea84e760a
steeltx <realsteel.gnu@gmail.com>
2021-08-07 20:42:44 -0500

    Middleware para guardar el body en logger

--- WebAPIAutores/Startup.cs
+++ WebAPIAutores/Startup.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System.IO;
 using System.Text.Json.Serialization;
 using WebAPIAutores.Servicios;
 using static WebAPIAutores.Servicios.IServicio;
@@ -54,8 +57,39 @@ namespace WebAPIAutores
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
+
+            app.Use(async (contexto, siguiente) =>
+            {
+                using (var ms = new MemoryStream())
+                {
+                    var cuerpoOriginalRespuesta = contexto.Response.Body;
+                    contexto.Response.Body = ms;
+
+                    await siguiente.Invoke();
+
+                    // despues de await , la informacion viene de regreso
+                    ms.Seek(0, SeekOrigin.Begin);
+                    string respuesta = new StreamReader(ms).ReadToEnd();
+                    ms.Seek(0, SeekOrigin.Begin);
+                    await ms.CopyToAsync(cuerpoOriginalRespuesta);
+
+                    contexto.Response.Body = cuerpoOriginalRespuesta;
+                    // mostrar en el log
+                    logger.LogInformation(respuesta);
+                }
+            });
+                
+            app.Map("/ruta1", app =>
+            {
+                app.Run(async contexto =>
+                {
+                    await contexto.Response.WriteAsync("Estoy interceptando la tuberia");
+                });
+            });
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
